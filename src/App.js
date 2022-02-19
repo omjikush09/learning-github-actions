31bc21371ce7f8d506631fb220a5a2bc2ef11e4d
Omji <omjimaurya09@gmail.com>
2022-02-19 17:42:39 +0530

    making some changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>Making some changes</p>
         </p>
         <a
           className="App-link"

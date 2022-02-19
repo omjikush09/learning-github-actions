d58458405e5a371d206e626e98f9c97acad24d1e
Omji <omjimaurya09@gmail.com>
2022-02-19 22:18:19 +0530

    making some changes extra

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           <p>Making some changes</p>
           <p>Making some changes more changes</p>
+          <p>Making some changes more changes</p>
         </p>
         <a
           className="App-link"
